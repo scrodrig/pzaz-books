f6063b27482c77f8028f68de87970c4885df77f9
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-19 21:38:53 +0000

    adds functionlity to store

--- pzaz_books_vue/src/store/index.js
+++ pzaz_books_vue/src/store/index.js
@@ -10,7 +10,24 @@ export default createStore({
     isLoading: false,
   },
   getters: {},
-  mutations: {},
+  mutations: {
+    initializeStore(state){
+      if(localStorage.getItem('cart')){
+        state.cart = JSON.parse(localStorage.getItem('cart'));
+      }else{
+        localStorage.setItem('cart', JSON.stringify(state.cart));
+      }
+    },
+    addToCart(state, item){
+      const exists = state.cart.items.filter(i => i.book.id === item.book.id);
+      if(exists.length){
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity);
+      }else{
+        state.cart.items.push(item);
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart));
+    },
+  },
   actions: {},
   modules: {},
 });
