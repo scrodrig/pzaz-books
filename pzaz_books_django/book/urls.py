d4eb535c7b8780f35d7e4fd1a7064026e0c34c90
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-20 15:02:52 +0000

    adds a post method for creating a book without image

--- pzaz_books_django/book/urls.py
+++ pzaz_books_django/book/urls.py
@@ -4,6 +4,8 @@ from book import views
 
 urlpatterns = [
   path("latest-books/", views.LatestBookList.as_view()),
+  path("all-books/", views.Books.as_view()),
+  path("books/create/", views.Books.as_view()),
   path("books/search/", views.search),
   path("books/<slug:category_slug>/<slug:book_slug>/", views.BookDetail.as_view()),
   path("books/<slug:category_slug>/", views.CategoryDetail.as_view()),
--- pzaz_books_django/book/views.py
+++ pzaz_books_django/book/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Q
 from django.http import Http404
-
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -21,11 +21,12 @@ class BookDetail(APIView):
             return Book.objects.filter(category__slug=category_slug).get(slug=book_slug)
         except Book.DoesNotExist:
             raise Http404
-        
+
     def get(self, request, category_slug, book_slug, format=None):
         book = self.get_object(category_slug, book_slug)
         serializer = BookSerializer(book)
         return Response(serializer.data)
+
 
 class CategoryDetail(APIView):
     def get_object(self, category_slug):
@@ -38,13 +39,43 @@ class CategoryDetail(APIView):
         category = self.get_object(category_slug)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
-    
-@api_view(['POST'])
+
+
+class Books(APIView):
+    def get_category(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        request_data = request.data
+        category_slug = request_data["category_slug"]
+        category = self.get_category(category_slug)
+        Book.objects.create(
+            category_id=category.id,
+            name=request_data["name"],
+            slug=request_data["slug"],
+            price=request_data["price"],
+            description=request_data["description"],
+        )
+        response_data = {"response": "Book created successfully"}
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
 def search(request):
-    query = request.data.get('query', '')
+    query = request.data.get("query", "")
     if query:
-        books = Book.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        books = Book.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
     else:
-        return Response({'books': []})
+        return Response({"books": []})
